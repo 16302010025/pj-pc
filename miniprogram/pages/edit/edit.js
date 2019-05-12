de6c300eb4cd2209095d4e77f14c9d93a43aaa1f
DESKTOP-8252MUP\Soul.专情 <16302010055@fudan.edu.cn>
2019-05-12 12:54:51 +0800

    微信小程序demo

---
+++ miniprogram/pages/edit/edit.js
@@ -0,0 +1,90 @@
+Page({
+  data: {
+    email: '',
+    name: '',
+    id: '',
+    gender: ["男", "女"],
+    genderindex:0,
+
+    college: ["清华大学","复旦大学","北京大学"],
+    collegeindex: 0,
+
+    isAgree: false
+  },
+  onLoad:function(){
+    let that = this;
+    wx.getStorage({
+      key: 'user',
+      success: function (res) {
+        that.setData({
+          email: res.data.email,
+          genderindex: res.data.genderindex,
+          name: res.data.name,
+          collegeindex: res.data.collegeindex,
+          id: res.data.id
+        })
+      }
+    })
+  },
+  showTopTips: function () {
+    var that = this;
+    this.setData({
+      showTopTips: true
+    });
+    setTimeout(function () {
+      that.setData({
+        showTopTips: false
+      });
+    }, 3000);
+  },
+
+  bindGenderChange: function (e) {
+    this.setData({
+      genderindex: e.detail.value
+    })
+  },
+
+  bindCollegeChange: function(e){
+    this.setData({
+      collegeindex: e.detail.value
+    })
+  },
+  bindAgreeChange: function (e) {
+    this.setData({
+      isAgree: !!e.detail.value.length
+    });
+  },
+  save:function(e){
+    wx.setStorage({
+      key: 'user',
+      data: {'email':this.data.email,
+      'genderindex':this.data.genderindex,
+      'name':this.data.name,
+      'collegeindex': this.data.collegeindex,
+      'id':this.data.id
+      }
+    })
+  },
+  emailInput: function (e) {
+    this.setData({
+      email: e.detail.value
+    })
+  },
+  nameInput: function (e) {
+    this.setData({
+      name: e.detail.value
+    })
+  },
+  idInput: function(e){
+    this.setData({
+      id: e.detail.value
+    })
+  },
+  saveInput:function(e){
+    let i = e.currentTarget.dataset.name;
+    this.setData({
+      i: e.detail.value
+    })
+    console.log(this.data.name)
+  }
+});
---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,89 @@
+// pages/home/home.js
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  data: {
+    tabs: ["课程推荐", "课程搜索","已选课程"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0
+  },
+  
+  
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: 0,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  }
+})
