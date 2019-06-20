f678dbd9605b61490b3d362bb4b072dfd732cb1d
DESKTOP-EUI2K0M\黄文浦 <16302010052@fudan.edu.cn>
2019-06-20 09:14:25 +0800

    测试通过注释

--- mini-program/src/main/java/education/client/student/controller/StudentController.java
+++ mini-program/src/main/java/education/client/student/controller/StudentController.java
@@ -12,41 +12,43 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("user")
 public class StudentController {
-    @Resource
-    private StudentService studentService;
+  @Resource
+  private StudentService studentService;
 
-    @PostMapping(value = "loginAuth", produces = "application/json;UTF-8")
-    public String login(@RequestParam() String code) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("openid",studentService.login(code));
-        return jsonObject.toJSONString();
-    }
-    //获得和完善用户信息的接口,用id取得学生全部个人信息
-    @RequestMapping(value = "getStu", produces = "application/json;UTF-8")
-    public String getStu(@RequestParam() String studentid) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("student",studentService.getStudentById(studentid));
-        return jsonObject.toJSONString();
-    }
+  //登录
+  @PostMapping(value = "loginAuth", produces = "application/json;UTF-8")
+  public String login(@RequestParam() String code) {
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("openid", studentService.login(code));
+    return jsonObject.toJSONString();
+  }
 
-    // 更新个人信息
-    @PostMapping(value = "updateStu", produces = "application/json;UTF-8")
-    public String updateStu(@RequestParam() String studentid, @RequestParam() String email, @RequestParam() String name,
-                             @RequestParam() boolean gender, @RequestParam() String studentNo) {
-        JSONObject jsonObject = new JSONObject();
-        Student student = new Student();
-        student.setStudentid(studentid);
-        student.setEmail(email);
-        student.setName(name);
-        student.setGender(gender);
-        student.setStudentno(studentNo);
-        Student checkedStudent = studentService.getStudentById(studentid);
-        if(null == checkedStudent){
-            jsonObject.put("flag",false);
-            return jsonObject.toJSONString();
-        }
-        studentService.update(student);
-        jsonObject.put("flag",true);
-        return jsonObject.toJSONString();
+  //获得和完善用户信息的接口,用id取得学生全部个人信息    已测试
+  @RequestMapping(value = "getStu", produces = "application/json;UTF-8")
+  public String getStu(@RequestParam() String studentid) {
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("student", studentService.getStudentById(studentid));
+    return jsonObject.toJSONString();
+  }
+
+  // 更新个人信息   已测试
+  @PostMapping(value = "updateStu", produces = "application/json;UTF-8")
+  public String updateStu(@RequestParam() String studentid, @RequestParam() String email, @RequestParam() String name,
+                          @RequestParam() boolean gender, @RequestParam() String studentNo) {
+    JSONObject jsonObject = new JSONObject();
+    Student student = new Student();
+    student.setStudentid(studentid);
+    student.setEmail(email);
+    student.setName(name);
+    student.setGender(gender);
+    student.setStudentno(studentNo);
+    Student checkedStudent = studentService.getStudentById(studentid);
+    if (null == checkedStudent) {
+      jsonObject.put("flag", false);
+      return jsonObject.toJSONString();
     }
+    studentService.update(student);
+    jsonObject.put("flag", true);
+    return jsonObject.toJSONString();
+  }
 }
