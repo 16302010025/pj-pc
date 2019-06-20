e4fcdd8ae4833c3933730dbd26588f1b2020994c
DESKTOP-8252MUP\Soul.专情 <16302010055@fudan.edu.cn>
2019-06-20 17:15:15 +0800

    add students mappers

--- mini-program/src/main/java/education/MiniProgramApplication.java
+++ mini-program/src/main/java/education/MiniProgramApplication.java
@@ -1,16 +1,30 @@
 package education;
 
+import education.dao.WXCustomMapper;
+import education.entity.wx.WXCourse;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
 
 
 @SpringBootApplication
 @MapperScan("education.dao")
+@RestController
 public class MiniProgramApplication {
+  @Resource
+  WXCustomMapper wxCustomMapper;
 
-    public static void main(String[] args) {
-        SpringApplication.run(MiniProgramApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MiniProgramApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public List<WXCourse> hello() {
+    return wxCustomMapper.getAllCourse();
+  }
 }
