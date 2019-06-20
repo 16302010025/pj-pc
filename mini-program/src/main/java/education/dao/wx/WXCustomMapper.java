3955568857a75ca36a0d7b531818d44767dd7526
DESKTOP-8252MUP\Soul.专情 <16302010055@fudan.edu.cn>
2019-06-20 11:37:38 +0800

    modify

--- mini-program/src/main/java/education/dao/wx/WXCustomMapper.java
+++ mini-program/src/main/java/education/dao/wx/WXCustomMapper.java
@@ -1,9 +1,8 @@
 package education.dao.wx;
 
-import education.entity.Chapter;
-import education.entity.Favorite;
-import education.entity.Teacher;
+import education.entity.*;
 import education.entity.wx.WXCourse;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -21,5 +20,10 @@ public interface WXCustomMapper {
 
   @Select("select * from favorite where studentID = #{studentID}")
   List<Favorite> getFavorite(String studentID);
-  
+
+  @Select("select exerciseID, description, correctOption, optionA, optionB, optionC, optionD from exercise natural join paper where courseID = #{courseID}")
+  List<ExerciseWithBLOBs> getPaper(String courseID);
+
+  @Insert("insert into dopaper (#{studentID}, #{exerciseID}, #{paperID}, #{choose})")
+  int insertDoPaper(@Param("studentID") String studentID, @Param("exerciseID") Integer exerciseID, @Param("paperID")Integer paperID , @Param("choose")Character choose);
 }
