5d55545f8ad42023447fcfdd72223925a14cdecf
DESKTOP-8252MUP\Soul.专情 <16302010055@fudan.edu.cn>
2019-06-21 18:49:58 +0800

    modify

--- mini-program/src/main/java/education/dao/TeacherExamMapper.java
+++ mini-program/src/main/java/education/dao/TeacherExamMapper.java
@@ -2,42 +2,27 @@ package education.dao;
 
 import education.entity.Exercise;
 import education.entity.Paper;
-import org.apache.ibatis.annotations.Insert;
-import org.springframework.web.servlet.tags.Param;
 
 import java.util.List;
 
 public interface TeacherExamMapper {
   /**
    *
-   * @param teacherID 教师ID
-   * @param courseID 课程ID
+   * @param paper
    * @return paperID
    */
   int addPaper(Paper paper);
 
   /**
    *
-   * @param paperID 试题ID
-   * @param description 题干
-   * @param correct 正确选项
-   * @param a
-   * @param b
-   * @param c
-   * @param d
+   * @param exercise
    * @return 试题ID
    */
   int addExercise(Exercise exercise);
 
   /**
    *
-   * @param paperID 试题ID
-   * @param description 题干
-   * @param correct 正确选项
-   * @param a
-   * @param b
-   * @param c
-   * @param d
+   * @param exercise
    * @return 是否成功
    */
   boolean updateExercise(Exercise exercise);
@@ -52,7 +37,7 @@ public interface TeacherExamMapper {
   /**
    *
    * @param courseID 课程ID
-   * @return 试卷对象一一对应
+   * @return 所有该course的所有paper
    */
   List<Paper> findPaperByCourseID(int courseID);
 
@@ -73,7 +58,7 @@ public interface TeacherExamMapper {
   /**
    *
    * @param paperID 试卷ID
-   * @return 所有题目的ID
+   * @return 所有题目
    */
   List<Exercise> findExerciseByPaperID(int paperID);
 }
